49a95019ff1edc4c3fb1b88402683a32e5a79adb
Maxim Khorkhordin <x.maxim84@mail.ru>
2022-01-05 18:59:44 +0300

    адаптированы все страницы кроме контактов

--- dist/js/script.js
+++ dist/js/script.js
@@ -14,3 +14,12 @@ close.addEventListener('click', () => {
     modal.classList.remove('active');
     close.classList.remove('active');
 });
+
+//mobile-menu
+const openMenu = document.querySelector('.hamburger'),
+      mobileMenu = document.querySelector('.header__menu');
+
+openMenu.addEventListener('click', () => {
+    mobileMenu.classList.toggle('header__menu_active');
+    openMenu.classList.toggle('hamburger_active');
+});
